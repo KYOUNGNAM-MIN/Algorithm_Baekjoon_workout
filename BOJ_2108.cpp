2866162e8034661b16f53155f553ab373c32d176
민경남 <alsrudska97@naver.com>
2021-02-01 16:04:46 +0900

    BOJ

--- BOJ_2108.cpp
+++ BOJ_2108.cpp
@@ -5,43 +5,38 @@
 using namespace std;
 
 int main(void){
-	int n;
-	int visit[8001] = {0,};
-	int maxN = -4000, minN = 4000;
-	int maxVisit = 0, maxCount;
-	cin >> n;
-	vector<int> number(n);
-	float sum = 0;
-	
-	for(int i=0; i<n; i++){
-		cin >> number[i];
-		sum += number[i];
-		visit[number[i]+4000]++;
-		maxN = max(maxN, number[i]);
-		minN = min(minN, number[i]);
-	}
-	
-	sort(number.begin(), number.end());
-	int curIdx = 0;
-	for(int i=0; i<8001; i++){
-		if(visit[i] > maxVisit){
-			maxVisit = visit[i];
-			curIdx = i;
-			maxCount = i-4000;
-		}
-	}
-	for(int i=0; i<8001; i++){
-		if((visit[i]==maxVisit) && (curIdx<i)){
-			maxCount = i-4000;
-			break;
-		}
-	}
-	
-	
-	cout << round(sum/n) << "\n";
-	cout << number[n/2] << "\n";
-	cout << maxCount << "\n";
-	cout << maxN-minN;
+	int N, temp, mode = 0, min, max = 0;
+	double mean;
+    bool isSecond = false;
+    cin >> N;
+    vector<int> vec(N);
+    vector<int> vec2(8001, 0);
+    for(int i = 0; i < N; i ++){
+        cin>>vec[i];
+        mean += vec[i];
+        temp = (vec[i] <= 0) ? abs(vec[i]) : vec[i] + 4000;
+        vec2[temp]++;
+        if(vec2[temp] > max)
+            max = vec2[temp];
+    }
+    sort(vec.begin(), vec.end());
+
+    for(int i = -4000; i < 4001; i++){
+        temp = i <= 0 ? abs(i) : i+4000;
+        if(vec2[temp] == max){
+            mode = i;
+            if(isSecond)
+                break;
+            isSecond = true;
+        }
+    }
+
+    cout<<round(mean/(double)N)<<'\n';
+    cout<<vec[round(N/2)]<<'\n';
+    cout<<mode<<'\n';
+    min = vec[0];
+    max = vec[vec.size()-1];
+    cout<<max - min<<'\n';
 	
 	
 	return 0;
